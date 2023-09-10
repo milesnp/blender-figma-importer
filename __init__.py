57593f2488cdd0d9a4495198261580d7e8d9ab37
Nick Miles <41658201+milesnp@users.noreply.github.com>
2023-09-10 14:19:54 -0500

    implement the image import

--- __init__.py
+++ __init__.py
@@ -11,8 +11,11 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+from io import BytesIO
+import os
 import requests
 import bpy
+
 bl_info = {
     "name": "Import Figma",
     "description": "imports figma files into blender",
@@ -24,14 +27,18 @@ bl_info = {
     "category": "Import-Export"
 }
 
-
-# Define the URL for the Figma API
-FIGMA_API_URL = "https://api.figma.com/v1"
-
-#api_key = REDACTED
-headers = {"X-Figma-Token": api_key, }
 # SHADELESS or EMISSION
 SHADER_OPTION = "EMISSION"
+# import scale divisor
+PX_TO_METER = 121.92  # 1200 DPI I think? Still ends up huge
+# Definite path to image folder if desired, otherwise the location of the\
+# .blend file will be used:
+#IMAGE_DIR = r"C:/temp/figma-images"
+IMAGE_DIR = None
+API_KEY = "YOUR_API_KEY"
+
+FIGMA_API_URL = "https://api.figma.com/v1"
+HEADERS = {"X-Figma-Token": API_KEY, }
 
 # authenticate with Figma (TODO if I want to use oauth2 and make this a legit project)
 # (user provides team ID, retrieve all team projects
@@ -50,7 +57,7 @@ class FigmaAddonPanel(bpy.types.Panel):
     bl_idname = "_PT_FigmaAddonPanel"
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
-    bl_category = 'Tools'
+    bl_category = 'Import'
 
     def draw(self, context):
         layout = self.layout
@@ -80,9 +87,9 @@ class FigmaAddonPanel(bpy.types.Panel):
             col.label(text="Select a Page:")
             col.prop(context.scene, "figma_selected_page", text="")
 
-            col.operator("figma.import_nodes", text="Import")
+            col.prop(context.scene, "figma_scale", text="Scale")
 
-        # if context.scene.figma_nodes:
+            col.operator("figma.import_nodes", text="Import")
 
 
 class FigmaRetrieveProjectsOperator(bpy.types.Operator):
@@ -94,7 +101,7 @@ class FigmaRetrieveProjectsOperator(bpy.types.Operator):
         team_id = context.scene.figma_team_id
         # Send a GET request to retrieve projects
         response = requests.get(
-            f"{FIGMA_API_URL}/teams/{team_id}/projects", headers=headers)
+            f"{FIGMA_API_URL}/teams/{team_id}/projects", headers=HEADERS)
 
         if response.status_code == 200:
             projects = response.json().get("projects", [])
@@ -125,7 +132,7 @@ class FigmaRetrieveFilesOperator(bpy.types.Operator):
         selected_project = context.scene.figma_projects[project]
 
         response = requests.get(
-            f"{FIGMA_API_URL}/projects/{selected_project.key}/files", headers=headers)
+            f"{FIGMA_API_URL}/projects/{selected_project.key}/files", headers=HEADERS)
         if response.status_code == 200:
             self.report({'INFO'}, str(response.content))
             files = response.json().get("files", [])
@@ -152,9 +159,7 @@ class FigmaRetrieveNodesOperator(bpy.types.Operator):
     def get_child_groups(self, groups):
         children = []
         for node in groups:
-            children = children + \
-                list(filter(lambda x: x['type'] ==
-                     "GROUP", node.get("children", {})))
+            children = children + node.get("children", [])
         return children
 
     def execute(self, context):
@@ -163,10 +168,11 @@ class FigmaRetrieveNodesOperator(bpy.types.Operator):
         selected_file = context.scene.figma_files[file]
 
         response = requests.get(
-            f"{FIGMA_API_URL}/files/{selected_file.key}", headers=headers)
+            f"{FIGMA_API_URL}/files/{selected_file.key}", headers=HEADERS)
         if response.status_code == 200:
             pages = response.json().get("document", {}).get("children", [])
             context.scene.figma_pages.clear()
+            context.scene.figma_nodes.clear()
             children_to_export = []
             for page in pages:
                 page_item = context.scene.figma_pages.add()
@@ -174,17 +180,31 @@ class FigmaRetrieveNodesOperator(bpy.types.Operator):
                 page_item.key = page['id']
 
                 looped_groups = []
-                new_child_groups = list(
-                    filter(lambda x: x['type'] == "GROUP", page.get("children", {})))
+                new_child_groups = page.get("children", {})
                 while new_child_groups:
                     looped_groups = looped_groups + new_child_groups
                     new_child_groups = self.get_child_groups(new_child_groups)
 
                 children_to_export = children_to_export + \
                     list(filter(lambda x: x['name'][0] == "_", looped_groups))
+                children_to_export = children_to_export + \
+                    list(filter(lambda x: x['name'][0] == "!", looped_groups))
 
                 self.report(
-                    {'INFO'}, f"Exportable group nodes retrieved from {page['name']}: {', '.join([group['name'] for group in children_to_export])}")
+                    {'INFO'}, f"Exportable nodes retrieved from {page['name']}: {', '.join([group['name'] for group in children_to_export])}")
+
+                for child in children_to_export:
+                    child_item = context.scene.figma_nodes.add()
+                    child_item.name = child['name']
+                    child_item.key = child['id']
+                    bounding_box = child['absoluteBoundingBox']
+                    child_item.x_bottom_left = bounding_box['x'] / PX_TO_METER
+                    child_item.y_bottom_left = -1 * \
+                        bounding_box['y'] / PX_TO_METER
+                    child_item.height = bounding_box['height'] / PX_TO_METER
+                    child_item.width = bounding_box['width'] / PX_TO_METER
+                    child_item.use_absolute_bb = child['name'][0] == "!"
+                    child_item.parent = page['id']
         else:
             self.report(
                 {'ERROR'}, f"Failed to retrieve nodes. Status code: {response.status_code}")
@@ -197,16 +217,123 @@ class FigmaImportNodesOperator(bpy.types.Operator):
     bl_label = "Import Nodes"
     bl_options = {'REGISTER', 'UNDO'}
 
+    def set_image_dir(self):
+        imagedir = IMAGE_DIR
+        if not imagedir:
+            blenddir = bpy.path.abspath('//')
+            if not blenddir:
+                self.report({'ERROR'}, "Save this file as a .blend first!")
+                return {'CANCELLED'}
+            imagedir = os.path.join(blenddir, "figma_images")
+        return imagedir
+
+    def get_image_name(self, node):
+        return f"{node.key}.png".replace(":", "_")
+
+    def request_images(self, context, nodes_to_import, use_absolute):
+        bb_string = "true" if use_absolute else "false"
+        imagedir = self.set_image_dir()
+        os.makedirs(imagedir, exist_ok=True)
+
+        ids_to_request = ','.join(group['key'] for group in nodes_to_import)
+        print(ids_to_request)
+        items = get_figma_files(self, context)
+        file = items[int(context.scene.figma_selected_file)][1]
+        selected_file = context.scene.figma_files[file]
+
+        response = requests.get(
+            f"{FIGMA_API_URL}/images/{selected_file.key}?ids={ids_to_request}&use_absolute_bounds={bb_string}", headers=HEADERS)
+        if response.status_code == 200:
+            images = response.json().get("images", {})
+            for node in nodes_to_import:
+                image_response = requests.get(images[node.key])
+                if response.status_code == 200:
+                    with open(os.path.join(imagedir, self.get_image_name(node)), 'wb') as img:
+                        img.write(image_response.content)
+                else:
+                    self.report(
+                        {'ERROR'}, f"Failed to download image. Status code: {response.status_code}")
+        else:
+            self.report(
+                {'ERROR'}, f"Failed to retrieve image URLs. Status code: {response.status_code}. {response.json().get('err')}")
+
+        #image_path = os.path.join(imagedir,f"{node.key}.png")
+
+    def import_plane(self, context, node):
+        self.report(
+            {"INFO"}, f"Importing {node.name} to {node.x_bottom_left}, {node.y_bottom_left} with {node.height} and {node.width}. abs: {str(node.use_absolute_bb)}")
+        imagedir = self.set_image_dir()
+        image_path = os.path.join(imagedir, self.get_image_name(node))
+
+        # Import the image as a plane
+        bpy.ops.import_image.to_plane(
+            shader=SHADER_OPTION,
+            align_axis="Z+",
+            files=[{"name": image_path}],
+            directory="",
+            relative=False,
+        )
+
+        height = node.height * context.scene.figma_scale
+        width = node.width * context.scene.figma_scale
+        x_bottom_left = node.x_bottom_left * context.scene.figma_scale
+        y_bottom_left = node.y_bottom_left * context.scene.figma_scale
+
+        # Get the last imported object (the plane)
+        plane = bpy.context.selected_objects[0]
+
+        plane.scale.x = 1
+        plane.scale.y = 1
+
+        # Calculate the scale factors to fit the bounding box
+        scale_x = width / plane.dimensions.x
+        scale_y = height / plane.dimensions.y
+        plane.scale.x = scale_x
+        plane.scale.y = scale_y
+
+        # Set the position of the plane based on the bottom-left coordinates
+        plane.location.x = x_bottom_left + (width / 2.0)
+        plane.location.y = y_bottom_left - (height / 2.0)
+
+        plane.name = node.name
+
     def execute(self, context):
-        print("figma balls")
+        # Ensure the "Images As Planes" add-on is enabled
+        bpy.ops.preferences.addon_enable(module="io_import_images_as_planes")
+
+        pages = get_figma_pages(self, context)
+        page = pages[int(context.scene.figma_selected_page)][1]
+        selected_file = context.scene.figma_pages[page]
+        nodes_absolute = list(
+            filter(lambda x: x.use_absolute_bb, context.scene.figma_nodes))
+        nodes_normal = list(
+            filter(lambda x: not x.use_absolute_bb, context.scene.figma_nodes))
+        nodes_to_import_absolute = list(
+            filter(lambda x: x.parent == selected_file.key, nodes_absolute))
+        nodes_to_import_normal = list(
+            filter(lambda x: x.parent == selected_file.key, nodes_normal))
+        self.request_images(context, nodes_to_import_absolute, True)
+        self.request_images(context, nodes_to_import_normal, False)
+        for node in nodes_to_import_absolute:
+            self.import_plane(context, node)
+        for node in nodes_to_import_normal:
+            self.import_plane(context, node)
+
         return {'FINISHED'}
 
 
 class FigmaItem(bpy.types.PropertyGroup):
     key: bpy.props.StringProperty(name="Figma ID")
     name: bpy.props.StringProperty(name="Figma Name")
-    parent: bpy.props.StringProperty(name="Parent Node")
-    selected: bpy.props.BoolProperty(name="Selected", default=False)
+
+
+class FigmaNode(FigmaItem):
+    parent: bpy.props.StringProperty(name="Parent Page")
+    x_bottom_left: bpy.props.FloatProperty(name="Bottom Left X")
+    y_bottom_left: bpy.props.FloatProperty(name="Bottom Left Y")
+    width: bpy.props.FloatProperty(name="width")
+    height: bpy.props.FloatProperty(name="height")
+    use_absolute_bb: bpy.props.BoolProperty(name="text?")
 
 
 class FigmaUnregisterOperator(bpy.types.Operator):
@@ -220,6 +347,7 @@ class FigmaUnregisterOperator(bpy.types.Operator):
 
 classes = (
     FigmaItem,
+    FigmaNode,
     FigmaAddonPanel,
     FigmaRetrieveFilesOperator,
     FigmaRetrieveProjectsOperator,
@@ -251,8 +379,6 @@ def get_figma_pages(self, context):
 
 
 def register():
-    # Ensure the "Images As Planes" add-on is enabled
-    # bpy.ops.preferences.addon_enable(module="io_import_images_as_planes")
 
     for cls in classes:
         bpy.utils.register_class(cls)
@@ -269,6 +395,9 @@ def register():
     bpy.types.Scene.figma_pages = bpy.props.CollectionProperty(type=FigmaItem)
     bpy.types.Scene.figma_selected_page = bpy.props.EnumProperty(
         items=get_figma_pages, name="Pages")
+    bpy.types.Scene.figma_nodes = bpy.props.CollectionProperty(type=FigmaNode)
+    bpy.types.Scene.figma_scale = bpy.props.FloatProperty(
+        name="Import Scale", default=1.0)
 
 
 def unregister():
@@ -282,6 +411,7 @@ def unregister():
     del bpy.types.Scene.figma_selected_file
     del bpy.types.Scene.figma_pages
     del bpy.types.Scene.figma_selected_page
+    del bpy.types.Scene.figma_nodes
 
 
 # This allows you to run the script directly from Blender's Text editor
